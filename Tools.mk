c28f39bc5f6ed610b0a3672eb5516483d62511b2
Cary Haynie <cary.haynie@gmail.com>
2016-07-10 14:33:07 -0400

    correctly configure ld and nasm paths on linux.

--- Tools.mk
+++ Tools.mk
@@ -3,7 +3,12 @@
 SHARD_TOOLCHAIN_TARGET := $(SHARD_ARCH)-$(SHARD_BINARY_FORMAT)
 
 # Absolute path to the linker
-LD := /d/Binaries/Binutils/bin/$(SHARD_TOOLCHAIN_TARGET)-ld.exe
-
-# Absolute path to the NASM executable
-NASM := /d/Binaries/NASM/nasm.exe
+ifeq ($(OS),Windows_NT)
+	LD := /d/Binaries/Binutils/bin/$(SHARD_TOOLCHAIN_TARGET)-ld.exe
+	# Absolute path to the NASM executable
+	NASM := /d/Binaries/NASM/nasm.exe
+endif
+ifeq ($(shell uname),Linux)
+	LD := $(shell which ld)
+	NASM := $(shell which nasm)
+endif
