f98d7978fc0a0b2eabcf78daf5e1d8a64737c4e9
Cary Haynie <cary.haynie@gmail.com>
2016-07-11 00:03:38 -0400

    add debugging module to write to vga buffer.

--- src/kernel/src/lib.rs
+++ src/kernel/src/lib.rs
@@ -1,23 +1,28 @@
+#![feature(const_fn)]
 #![feature(lang_items)]
+#![feature(unique)]
 #![no_std]
 
 extern crate rlibc;
+extern crate spin;
+
+#[macro_use]
+mod debug;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    let hello = b"Hello, World!";
-    let color_byte = 0x1f;
 
-    let mut hello_colored = [color_byte; 26];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
-    }
-
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
+    debug::vga::clear_screen();
+    println!("Hello, World!");
 
     loop {}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
 #[lang = "panic_fmt"] extern fn panic_fmt() -> ! { loop {} }
+
+#[allow(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn _Unwind_Resume() -> ! {
+    loop {}
+}
