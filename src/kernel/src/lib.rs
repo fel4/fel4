84fec7725f9b787e39e6601a5657886a54ceb4ed
Cary Haynie <cary.haynie@gmail.com>
2016-07-10 22:48:42 -0400

    Hello world from rust.

--- src/kernel/src/lib.rs
+++ src/kernel/src/lib.rs
@@ -5,8 +5,18 @@ extern crate rlibc;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    let x = ["Hello", " ", "World", "!"];
-    let test = (0..3).flat_map(|x| 0..x).zip(0..);
+    let hello = b"Hello, World!";
+    let color_byte = 0x1f;
+
+    let mut hello_colored = [color_byte; 26];
+    for (i, char_byte) in hello.into_iter().enumerate() {
+        hello_colored[i*2] = *char_byte;
+    }
+
+    let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    unsafe { *buffer_ptr = hello_colored };
+
+    loop {}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
