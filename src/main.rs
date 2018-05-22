71f7ebf1af83b4e1d80cfa8074faf5ff0fa25689
Cary Haynie <cary.haynie@gmail.com>
2018-05-21 22:21:14 -0700

    update to use cargo-xbuild and add unit testing to vga buffer.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 #![feature(const_fn)]
 #![feature(lang_items)] // required for defining the panic handler
 #![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![cfg_attr(not(test), no_main)] // disable all Rust-level entry points
+#![cfg_attr(test, allow(dead_code, unused_macros))]
 
 #[macro_use]
 extern crate lazy_static;
 extern crate spin;
 extern crate volatile;
 
+#[cfg(test)]
+extern crate array_init;
+#[cfg(test)]
+extern crate std;
+
 #[macro_use]
 mod vga_buffer;
 
+#[cfg(not(test))]
 #[lang = "panic_fmt"] // define a function that should be called on panic
 #[no_mangle]
 pub extern "C" fn rust_begin_panic(
@@ -22,6 +29,7 @@ pub extern "C" fn rust_begin_panic(
     loop {}
 }
 
+#[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     //vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
