ca374c88044b7f8e7d0c33e454758fcc33846d9e
Cary Haynie <cary.haynie@gmail.com>
2018-10-12 22:30:30 -0700

    remove old panic attributes

--- src/bin/test-double-fault-exception.rs
+++ src/bin/test-double-fault-exception.rs
@@ -1,6 +1,5 @@
 #![feature(abi_x86_interrupt)]
 #![feature(const_fn)]
-#![feature(panic_implementation)] // required for defining the panic handler
 #![no_std] // don't link the Rust standard library
 #![cfg_attr(not(test), no_main)] // disable all Rust-level entry points
 #![cfg_attr(test, allow(dead_code, unused_macros))]
@@ -56,9 +55,8 @@ pub extern "C" fn _start() -> ! {
 }
 
 #[cfg(not(test))]
-#[panic_implementation] // define a function that should be called on panic
-#[no_mangle]
-pub fn panic(
+#[panic_handler] // define a function that should be called on panic
+fn panic(
     _info: &PanicInfo
 ) -> ! {
     loop {}
