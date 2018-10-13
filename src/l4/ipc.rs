dc943c73b77ea132a1ee212538ef8f5259d62e02
Cary Haynie <cary.haynie@gmail.com>
2018-10-13 01:52:21 -0700

    add sel4 core system calls

--- src/l4/ipc.rs
+++ src/l4/ipc.rs
@@ -1,2 +1,4 @@
 // Note structure must by 512-byte aligned (implies sizeof(IPCBuffer) == 512)
 pub struct IPCBuffer {}
+
+pub struct MessageInfo {}
--- src/l4/mod.rs
+++ src/l4/mod.rs
@@ -14,3 +14,43 @@ impl Untyped {
         unimplemented!();
     }
 }
+
+// core sel4 syscalls
+
+/// Send to a capability
+pub fn send(dest: caps::CPtr, msg_info: ipc::MessageInfo) {
+    unimplemented!();
+}
+/// Block until a message is received on an endpoint.
+pub fn recv(src: caps::CPtr, sender: Option<*mut usize>) -> ipc::MessageInfo {
+    unimplemented!();
+}
+/// Call a capability.
+pub fn call(dest: caps::CPtr, msg_info: ipc::MessageInfo) -> ipc::MessageInfo {
+    unimplemented!();
+}
+/// Perform a send to a one-o reply capability stored when the thread was last called.
+pub fn reply(msg_info: ipc::MessageInfo) {
+    unimplemented!();
+}
+/// Perform a non-blocking send to a capability.
+pub fn nb_send(dest: caps::CPtr, msg_info: ipc::MessageInfo) {
+    unimplemented!();
+}
+/// Perform a reply followed by a receive in one system call
+pub fn reply_recv(
+    dest: caps::CPtr,
+    msg_info: ipc::MessageInfo,
+    sender: Option<*mut usize>,
+) -> ipc::MessageInfo {
+    unimplemented!();
+}
+/// Receive a message from an endpoint but do not block in the case that no messages are
+/// pending.
+pub fn nb_recv(src: caps::CPtr, sender: Option<*mut usize>) -> ipc::MessageInfo {
+    unimplemented!();
+}
+/// Donate the remaining timeslice to a thread of the same priority.
+pub fn yield_thread() {
+    unimplemented!();
+} // yield is a keyword in rust.
